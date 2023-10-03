99541f5a9c92119978a1b13bb83dfb796d2ed7ba
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2023-10-03 20:45:03 -0300

    Adição de pacotes NuGet.
    Os pacotes NuGet contendo as bibliotecas necessárias para o desenvolvimento do projeto foram adicionados.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace code_wizards_website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

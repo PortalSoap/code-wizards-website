024a50dd52324b1efd8eef8f007023d45550bc2c
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2023-10-08 19:03:33 -0300

    Exclusão de arquivos desnecessários
    Os arquivos advindos com a template que não foram utilizados e nem serão utilizados em futuras versões foram excluídos.

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace code_wizards_website.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
